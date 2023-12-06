f0e23efad8a83d7a6e7b7ff12a4ac8cf3ced73c1
David Adams <davida@dugeo.com>
2023-12-06 16:26:43 +0800

    fix day1 part 2

--- 2023/day-01/src/bin/parts2.rs
+++ 2023/day-01/src/bin/parts2.rs
@@ -1,12 +1,11 @@
 use std::collections::HashMap;
+use regex::Regex; 
 
 fn main() {
-    let input = include_str!("input2.txt");
+    let input = include_str!("input1.txt");
     let keywords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
     let mut total = 0;
-
     let map: HashMap<&str, i32> = [
         ("zero", 0),
         ("one", 1),
@@ -18,37 +17,52 @@ fn main() {
         ("seven", 7),
         ("eight", 8),
         ("nine", 9),
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
     ]
     .iter()
     .cloned()
     .collect();
-
     for line in input.lines() {
-        let mut first_occurrence = line.len(); // Initialize to a value larger than any possible index
+        let mut first_occurrence = line.len(); 
         let mut last_occurrence = 0;
         let mut first_keyword = "";
         let mut last_keyword = "";
 
         for keyword in keywords.iter().chain(digits.iter()) {
-            if let Some(start) = line.find(keyword) {
-                if start < first_occurrence {
-                    first_occurrence = start;
+            let v: Vec<_> = line.match_indices(keyword).collect(); 
+
+            if !v.first().is_none() {
+                let f = v.first().unwrap().0;
+                if f <= first_occurrence {
+                    first_occurrence = f;
                     first_keyword = keyword;
-                    println!("{}", first_occurrence);
-                    println!("{}", first_keyword);
                 }
-            
-                if start > last_occurrence {
-                    last_occurrence = start;
-                    last_keyword = keyword;
-                    println!("{}", last_occurrence);
-                    println!("{}", last_keyword);
-                }
+            } else {
+                continue;
             }
+            let mut potential_last = 0; 
+            if !v.last().is_none() {
+                potential_last = v.last().unwrap().0;
+            } else if !v.first().is_none() {
+                potential_last = v.first().unwrap().0;
+            }
+            if potential_last >= last_occurrence {
+                last_occurrence = potential_last;
+                last_keyword = keyword;
+            }
+
         }
-
-        total += map.get(first_keyword).unwrap_or(&0) + map.get(last_keyword).unwrap_or(&0);
+        let line_total = 10*map.get(first_keyword).unwrap_or(&0) + map.get(last_keyword).unwrap_or(&0);
+        total += line_total
     }
-
     println!("{}", total);
 }
